8429ccfd7edee4fca1e7eb131659fc44be0bd764
isbndkdf <beauiful123@naver.com>
2016-11-03 17:50:07 +0900

    example of sample in R

---
+++ My Lotto Numbers.R
@@ -0,0 +1,8 @@
+# This is for generating my Lotto Numbers
+# Created by Choi Seoung Hyck
+# 9/9 2016
+
+x= 1:45
+numbers = sample(x, size = 6, replace = FALSE)
+print("My Lotto Numbers Are: ")
+print(numbers)
